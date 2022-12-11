03b3bee102bb1c1c12a7b415e85cf1fd9d44a39b
Djilani CARDINEAU <djilani.cardineau@epitech.eu>
2022-12-11 21:02:13 +0100

    fix

--- src/new-darkelle-project/index.ts
+++ src/new-darkelle-project/index.ts
@@ -1,12 +1,23 @@
-import { apply, chain, externalSchematic, MergeStrategy, mergeWith, Rule, SchematicContext, strings, template, Tree, url } from '@angular-devkit/schematics';
-
+import {
+  apply,
+  chain,
+  externalSchematic,
+  MergeStrategy,
+  mergeWith,
+  Rule,
+  SchematicContext,
+  strings,
+  template,
+  Tree,
+  url,
+} from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function newRepo(_options: any): Rule {
   const name = _options.name
   console.log('The name of the repo will be', name)
-  return (tree: Tree, _context: SchematicContext) => {
+  return async (tree: Tree, _context: SchematicContext) => {
 
     const templateSource = apply(url('./files'), [
       template({..._options, ...strings}),
@@ -14,13 +25,34 @@ export function newRepo(_options: any): Rule {
     const merged = mergeWith(templateSource, MergeStrategy.Overwrite)
 
     const rule = chain([
+      print("Generate repo"),
       generateRepo(name),
       merged,
-      updatePackageJson(name)
+      print("Add Material"),
+      addMaterial(),
+      print("Add PWA"),
+      addPwa(name),
+      print("Update package.json"),
+      updatePackageJson(name),
+      print("Create file and folder")
     ]);
 
     return rule(tree, _context) as Rule;
   };
+}
+
+function printTree(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    console.log(tree)
+    return tree
+  }
+}
+
+function print(message: string): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info(message);
+    return tree
+  }
 }
 
 function generateRepo(name: string): Rule {
@@ -35,6 +67,18 @@ function generateRepo(name: string): Rule {
   });
 }
 
+function addPwa(name: string): Rule {
+  return externalSchematic('@angular/pwa', 'ng-add', { project: name }, {scope: '/' + name});
+}
+
+function addMaterial(): Rule {
+  return externalSchematic('@angular/material', 'ng-add', {
+    theme: "deeppurple-amber",
+    typography: true,
+    animations: "enabled"
+  });
+}
+
 function updatePackageJson(name: string): Rule {
   return (tree: Tree, _: SchematicContext): Tree => {
     const path = `/${name}/package.json`;
@@ -43,13 +87,12 @@ function updatePackageJson(name: string): Rule {
 
     json.scripts = {
       ...json.scripts,
-      'build:prod': 'ng build --prod',
-      test: 'ng test --code-coverage',
-      lint: 'ng lint --fix',
+      "pwa": "http-server -p 8081 -c-1 dist/" + name,
     };
 
-    json.devDependencies.prettier = '^2.0.0';
-    json.devDependencies.husky = '^4.2.'
+    json.dependencies['http-server'] = '^14.1.1';
+    json.dependencies['ngx-toastr'] = '^16.0.1';
+    json.dependencies['dayjs'] = "^1.11.6";
 
     tree.overwrite(path, JSON.stringify(json, null, 2));
     return tree;
